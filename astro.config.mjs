f9cbf740461d25ec7f9cea6099d83de2661c32ed
keegooroomie <spartan121@bk.ru>
2026-03-03 18:58:13 +0700

    UPD/ add deploy routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Автоматически подстраиваем base под имя репозитория GitHub
+  // Локально (npm run dev) GITHUB_REPOSITORY не задан, поэтому base = undefined.
+  base: process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}`
+    : undefined,
+});
